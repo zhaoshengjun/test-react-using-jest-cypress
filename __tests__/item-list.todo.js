a62e4276dc2f4678456f757d34dffea1648bc602
zhaoshengjun <zhao.shengjun@gmail.com>
2018-05-21 23:00:30 +1000

    minimum setup for react testing using jest

---
+++ __tests__/item-list.todo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ItemList from "../item-list";
+
+test('should render "no items" when passing empty list', () => {
+  const container = document.createElement("div");
+  ReactDOM.render(<ItemList items={[]} />, container);
+  expect(container.innerHTML).toBe("no items");
+});
