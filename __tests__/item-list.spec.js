222bdc4f5596dc0d4f515194d99320d9f53b0e15
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-22 11:30:30 +1000

    initial setup for calculator app

--- __tests__/item-list.spec.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ItemList from "../item-list";
-
-test('should render "no items" when passing empty list', () => {
-  const container = document.createElement("div");
-  ReactDOM.render(<ItemList items={[]} />, container);
-  expect(container.textContent).toBe("no items");
-});
